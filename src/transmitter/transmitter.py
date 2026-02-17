3bc582fc03219967afa0ccecffc1f988a51ef5c8
Andi Dicu <andi.dicu@protonmail.com>
2026-02-17 23:29:51 +0000

    Simplify API call

--- src/transmitter/transmitter.py
+++ src/transmitter/transmitter.py
@@ -5,7 +5,6 @@ from src.core.types.property import Property
 from src.core.eligibility_checker.eligibility_checker import PropertyScoreAttributes
 
 ENDPOINT = os.getenv("NOTIFICATION_ENDPOINT")
-PATH_TO_FAVICON = 'https://static.vecteezy.com/system/resources/thumbnails/022/750/436/small/3d-home-icon-free-png.png'
 
 
 class Transmitter:
@@ -16,12 +15,14 @@ class Transmitter:
             "Filename": "property.jpg",
             "Title": property.address.strip(),
             "Priority": "5",
-            "Icon": PATH_TO_FAVICON,
             "Tags": "house_with_garden"
         }
 
     def _make_ntfy_data(self, property: Property, property_score_attributes: PropertyScoreAttributes):
-        return '\n'.join([f"€{property.price} p.m.", f"{property_score_attributes.distance_in_minutes} minutes away"]).encode(encoding='utf-8')
+        return '\n'.join([
+            f"€{property.price} p.m.",
+            f"{property_score_attributes.distance_in_minutes} minutes away"
+        ]).encode(encoding='utf-8')
 
     def transmit(self, property: Property, property_score_attributes: PropertyScoreAttributes):
         requests.post(
